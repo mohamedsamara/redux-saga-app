8b75a40345fa24591e3fee2a7291b836d77d2c02
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-03-18 00:56:30 -0700

    configure & build webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * index.js
+ * This is the entry file for the application, only render App component.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Redux Saga Boilerplate Works!!!</h1>,
+  document.getElementById('root')
+);
