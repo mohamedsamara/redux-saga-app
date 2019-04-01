dd046cfb7c1ab5df662f5786ecbe1a6a334edb4b
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-04-01 10:53:12 -0700

    add jest & enzyme

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  clearMocks: true,
+
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+
+  coverageDirectory: 'coverage',
+
+  moduleFileExtensions: ['js', 'json', 'jsx'],
+
+  setupFiles: ['<rootDir>/enzyme.config.js'],
+
+  testEnvironment: 'jsdom',
+
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '.+\\.(css|styl|less|sass|scss)$':
+      '<rootDir>/node_modules/jest-css-modules-transform'
+  },
+
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+
+  testURL: 'http://localhost',
+
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+
+  verbose: true
+};
